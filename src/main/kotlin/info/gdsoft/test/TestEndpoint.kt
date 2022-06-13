d5b192dccb46a781a69c640d1850d1661d9d014d
Jan Undrych <jan.undrych@gdsoft.info>
2022-06-13 06:10:47 +0200

    feat: add a node probe

--- src/main/kotlin/info/gdsoft/test/TestEndpoint.kt
+++ src/main/kotlin/info/gdsoft/test/TestEndpoint.kt
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class TestEndpoint {
 
+    companion object {
+        private val ID = (0..100_000_000).random()
+    }
+
     @Autowired
     private lateinit var template: R2dbcEntityTemplate
 
@@ -16,6 +20,6 @@ class TestEndpoint {
         .select(Strs::class.java)
         .all()
 
-
-
+    @GetMapping("/node-probe")
+    fun probe() = ID
 }
